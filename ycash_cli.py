86187bae5cdb83642cd96ffba9d186b8ca35d1e5
nultinator <72562693+nultinator@users.noreply.github.com>
2021-04-16 23:54:02 -0400

    Add files via upload

---
+++ ycash_cli.py
@@ -0,0 +1,5 @@
+from bitcoinrpc.authproxy import AuthServiceProxy
+user = input("user: ")
+password = input("password: ")
+access = AuthServiceProxy("http://" + user + ":" + password + "@127.0.0.1:8832")
+getblockchaininfo = access.getblockchaininfo()
