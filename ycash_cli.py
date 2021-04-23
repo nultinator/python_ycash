3fbe0c7f2a914f2101fb911a98fe61eeda6fbe33
nultinator <72562693+nultinator@users.noreply.github.com>
2021-04-22 23:04:55 -0400

    Update ycash_cli.py

--- ycash_cli.py
+++ ycash_cli.py
@@ -1,7 +1,8 @@
 from bitcoinrpc.authproxy import AuthServiceProxy
 user = input("user: ")
 password = input("password: ")
-access = AuthServiceProxy("http://" + user + ":" + password + "@127.0.0.1:8832")
+port = input("port: ")
+access = AuthServiceProxy("http://" + user + ":" + password + "@127.0.0.1:" + port)
 getblockchaininfo = access.getblockchaininfo()
 getnewaddress = access.getnewaddress()
 getbestblockhash = access.getbestblockhash()
